591a66bcff26fb14420aa272b7fe67ccde1136b1
Harindu Korala <harinduphluker@gmail.com>
2019-02-11 20:15:56 +1100

    Added I Reducer File

---
+++ OrleansIoTPlatform/GrainInterfaces/IReducer.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace OrleansIoTPlatform.GrainInterfaces
+{
+    public interface IReducer : Orleans.IGrainWithIntegerKey
+    {
+        Task ReduceAccelo(AccelerationDataPerSec subResults);
+        Task<AccelerationDataPerSec> GetResultsAccelo();
+    }
+}
